2c78a8b830c7799c384c2e4810deaa4ec8f86dc6
padilha.luisthiago <padilha.luisthiago@gmail.com>
2020-03-28 11:29:44 -0300

    rota de login ok

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,23 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async create(request, response, next){
+        try {
+            console.log(request.body);
+            const {id} = request.body;
+            const ong = await connection('ongs')
+                .where('id', id)
+                .select('name')
+                .first();
+                if(!ong){
+                    return response.status(400).json({
+                        error: 'No ONG found with this ID.'
+                    });
+                }
+                return response.json(ong);
+        } catch (err) {
+            next(err);
+        }
+        
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/profile', ProfileController.index);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
