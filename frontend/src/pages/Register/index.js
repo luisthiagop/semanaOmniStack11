08da6dbfb66683ef24084f6aad7037a5d3bcad1d
padilha.luisthiago <padilha.luisthiago@gmail.com>
2020-03-29 11:21:03 -0300

    ajustes frontend e cadastro de novos casos

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -18,7 +18,7 @@ export default function Register() {
 
                     <Link className="back-link" to="/">
                         <FiArrowLeft size="16" color="#e02041"/>
-                        Não tenho cadastro
+                        Voltar
                     </Link>
                 </section>
 
